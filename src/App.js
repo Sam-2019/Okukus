a891a1e2ee0c8395283c10efa26a4dd9d018f2bc
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-08-10 15:05:31 +0000

    Removed sensitive files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import Okukus from "./Okukus/Okukus";
-import Cart from './Okukus/Cart/Cart'
 import "bootstrap/dist/css/bootstrap.css";
 
 export default class App extends Component {
