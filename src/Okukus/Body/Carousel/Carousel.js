a25d429e872d73be9cbeedd64df099aeb80fc73d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-08-17 19:08:17 +0000

    Sourcing carousel images from Okukus server

--- src/Okukus/Body/Carousel/Carousel.js
+++ src/Okukus/Body/Carousel/Carousel.js
@@ -1,33 +1,28 @@
 import React, { Component } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import okuks0 from "./okuks0.jpg";
-import okuks1 from "./okuks1.jpg";
-import okuks2 from "./okuks2.jpg";
-import okuks3 from "./okuks3.jpg";
-import okuksslider from "./okuksslider.png";
 
 export default class Slider extends React.Component {
   render() {
     return (
       <Carousel indicators={false}>
         <Carousel.Item>
-          <img className="d-block w-100" src={okuks0} alt=" slide" />
+          <img className="d-block w-100" src='https://www.okukus.com/img/slides/okuks0.jpg' alt=" slide" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="d-block w-100" src={okuks1} alt=" slide" />
+          <img className="d-block w-100" src='http://okukus.com/img/slides/okuks1.jpg' alt=" slide" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="d-block w-100" src={okuks2} alt=" slide" />
+          <img className="d-block w-100" src='http://okukus.com/img/slides/okuks2.jpg' alt=" slide" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="d-block w-100" src={okuks3} alt=" slide" />
+          <img className="d-block w-100" src='http://okukus.com/img/slides/okuks3.jpg' alt=" slide" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img className="d-block w-100" src={okuksslider} alt="Third slide" />
+          <img className="d-block w-100" src='http://okukus.com/img/slides/okuksslider.png' alt="Third slide" />
         </Carousel.Item>
       </Carousel>
     );
