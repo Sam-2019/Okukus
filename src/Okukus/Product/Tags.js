807ca99ce4b6f4127fd338bad5388a02f6c90897
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-08-18 20:27:47 +0000

    Sidebar working now

--- src/Okukus/Product/Tags.js
+++ src/Okukus/Product/Tags.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import View from "../Body/View";
 import "./product.css";
 import axios from "axios";
 
@@ -20,7 +20,6 @@ const Tag = (props) => {
         data: formData,
         headers: { "Content-Type": "multipart/form-data" },
       }).then((res) => {
-
         setProduct(res.data);
       });
     };
@@ -54,25 +53,3 @@ const Tag = (props) => {
 };
 
 export default Tag;
-
-const View = (props) => {
-  return (
-    <div className=" product">
-      <NavLink to={/product/ + props.id} className="product_link">
-        <div className=" text-center">
-          <img
-            src={`https://okukus.com/${props.cover_photo_url}`}
-            alt="hi"
-            className="product-image"
-          />
-        </div>
-
-        <div className="product-name">{props.product_name}</div>
-
-        <div className="buy_button">
-          <button className="price">₵{props.unit_price}</button>
-        </div>
-      </NavLink>
-    </div>
-  );
-};
