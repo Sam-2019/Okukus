014f9b257968e107af23fe4432d4b98f190024aa
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-08-26 18:24:10 +0000

    Auth added

--- src/Okukus/Order/Confirm.js
+++ src/Okukus/Order/Confirm.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { auth } from "../User/authContext";
 import "./confirm.css";
 import { NavLink } from "react-router-dom";
 
 const Confirm = () => {
+  const { rootState } = useContext(auth);
+  const { firstName, lastName } = rootState;
   return (
     <div className=" ">
       <div className="confirm_form">
@@ -16,7 +19,10 @@ const Confirm = () => {
         </NavLink>
         <h2>Your Order Has Been Placed</h2>
         <h5>
-          Hi <code>username</code>
+          Hi{" "}
+          <code>
+            {firstName} {lastName}
+          </code>
         </h5>
         <p>
           Thank you for shopping with us! Your order <code>order number</code>{" "}
