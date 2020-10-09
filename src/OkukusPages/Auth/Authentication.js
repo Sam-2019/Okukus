b5df01234e443a421ebbe7034d340d133f9a01a5
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-10-09 15:27:40 +0000

    update

--- src/OkukusPages/Auth/Authentication.js
+++ src/OkukusPages/Auth/Authentication.js
@@ -24,7 +24,7 @@ import {
 import { axiosMethod } from "../helpers";
 
 const Authentication = () => {
-  const [Auth, setAuth] = useState(true);
+  const [Auth, setAuth] = useState(false);
   const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
