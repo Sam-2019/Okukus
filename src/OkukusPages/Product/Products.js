5b1658c09c2935d17850dbbcd6cf0dcddf8c781f
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-10-14 04:14:54 +0000

    Reverted Products page

--- src/OkukusPages/Product/Products.js
+++ src/OkukusPages/Product/Products.js
@@ -1,74 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import useInfiniteScroll from "../useInfinite";
+import React from "react";
 import View from "../Container/View/View";
 import Spinner from "../Spinner/Spinner";
+import { useAsyncc } from "../helpers";
+import { useAuthentication } from "../Auth/Context";
 
-const Product = () => {
-  const [data, setData] = useState([]);
-  const [offset, setOffset] = useState(0);
+const Products = () => {
+  const { getItems } = useAuthentication();
 
-  let url = `https:okukus.com/api_call_dev/get_books.php`;
+  const resource = useAsyncc(getItems);
 
-  const loadData = () => {
-    var formData = new FormData();
-    formData.set("offset", offset);
-
-    axios({
-      method: "post",
-      url: url,
-      data: formData,
-      headers: { "Content-Type": "multipart/form-data" },
-    })
-      .then((response) => {
-        setData(response.data);
-
-        response.data.forEach((d) => {
-          setOffset(offset + 1);
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const moreData = () => {
-    var formData = new FormData();
-    formData.set("offset", offset);
-
-    axios({
-      method: "post",
-      url: "https://okukus.com/api_call_dev/get_books.php",
-      data: formData,
-      headers: { "Content-Type": "multipart/form-data" },
-    })
-      .then((response) => {
-        setData([...data, ...response.data]);
-
-        response.data.forEach((d) => {
-          setOffset(offset + response.data.length);
-        });
-
-        setIsFetching(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const [isFetching, setIsFetching] = useInfiniteScroll(moreData);
-  useEffect(() => {
-    loadData();
-  }, []);
-
-  useEffect(() => {
-    moreData();
-  }, []);
-
-  if (data.length === 0) {
-    return <Spinner />;
-  }
-
-  let content = data.map(
+  let content = resource.value.map(
     ({ unique_id, unit_price, product_name, cover_photo_url }) => (
       <View
         key={unique_id}
@@ -81,10 +22,18 @@ const Product = () => {
   );
 
   return (
-    <div className="products_wrapper">
-      <div className="wrapper">{content}</div>
+    <div>
+      {resource.loading ? (
+
+          <Spinner />
+    
+      ) : (
+        <div className="wrapper">{content}</div>
+      )}
+
+      {/* {message ? <div className="message_wrapper ">{message} </div> : null} */}
     </div>
   );
 };
 
-export default Product;
+export default Products;
