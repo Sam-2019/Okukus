39cf46909eb77bdf7372e4c4ca3d5cf87b634729
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-10-19 15:53:55 +0000

    Account Reset Test

--- src/OkukusPages/User/AccountVerify.js
+++ src/OkukusPages/User/AccountVerify.js
@@ -42,65 +42,65 @@ const AccountVerify = (props) => {
 
 export default AccountVerify;
 
-// const NewPassword = ({ email }) => {
-//   const { updateUserPassword } = useAuthentication();
+const NewPassword = ({ email }) => {
+   const { updateUserPassword } = useAuthentication();
 
-//   const [newPassword, setNewPassword] = useState();
-//   const [confirmPassword, setConfirmPassword] = useState();
+   const [newPassword, setNewPassword] = useState();
+   const [confirmPassword, setConfirmPassword] = useState();
 
-//   const [message, setMessage] = useState();
+   const [message, setMessage] = useState();
 
-//   console.log(email);
+   console.log(email);
 
-//   const reset = () => {
-//     setNewPassword("");
-//     setConfirmPassword("");
-//   };
+   const reset = () => {
+     setNewPassword("");
+     setConfirmPassword("");
+   };
 
-//   const updatePassword = async (event) => {
-//     setMessage();
-//     event.preventDefault();
-//     var formData = new FormData();
+   const updatePassword = async (event) => {
+     setMessage();
+     event.preventDefault();
+     var formData = new FormData();
 
-//     formData.set("buyer_unique_id", email);
-//     formData.set("new_password", newPassword);
-//     formData.set("confirm_password", confirmPassword);
+     formData.set("buyer_unique_id", email);
+     formData.set("new_password", newPassword);
+     formData.set("confirm_password", confirmPassword);
 
-//     const data = await updateUserPassword(formData);
+     const data = await updateUserPassword(formData);
 
-//     if (data.data.error === true) {
-//       setMessage(data.data.message);
-//     } else if (data.data.error === false) {
-//       reset();
-//     }
-//   };
-//   return (
-//     <div>
-//       <div className="page_title">Reset Password</div>
+     if (data.data.error === true) {
+       setMessage(data.data.message);
+     } else if (data.data.error === false) {
+       reset();
+     }
+   };
+   return (
+     <div>
+       <div className="page_title">Reset Password</div>
 
-//       <Input
-//         classname="input "
-//         placeholder="New Password"
-//         value={newPassword}
-//         type="password"
-//         action={(e) => setNewPassword(e.target.value)}
-//       />
+       <Input
+         classname="input "
+         placeholder="New Password"
+         value={newPassword}
+         type="password"
+         action={(e) => setNewPassword(e.target.value)}
+       />
 
-//       <Input
-//         classname="input "
-//         placeholder="Confirm Password"
-//         value={confirmPassword}
-//         type="password"
-//         action={(e) => setConfirmPassword(e.target.value)}
-//       />
+       <Input
+         classname="input "
+         placeholder="Confirm Password"
+         value={confirmPassword}
+         type="password"
+         action={(e) => setConfirmPassword(e.target.value)}
+       />
 
-//       <div className="message_wrapper ">
-//         {message ? <Message message={message} classname="message" /> : null}
-//       </div>
+       <div className="message_wrapper ">
+         {message ? <Message message={message} classname="message" /> : null}
+       </div>
 
-//       <div className="button_wrapper ">
-//         <Button name="Submit" action={updatePassword} classname="primary" />
-//       </div>
-//     </div>
-//   );
-// };
+       <div className="button_wrapper ">
+         <Button name="Submit" action={updatePassword} classname="primary" />
+       </div>
+     </div>
+   );
+ };
