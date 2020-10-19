d6464464585badced0e77c92b6c716931b282124
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-10-19 14:38:26 +0000

    Account Reset Test

--- src/OkukusPages/User/AccountVerify.js
+++ src/OkukusPages/User/AccountVerify.js
@@ -3,17 +3,16 @@ import { useParams } from "react-router-dom";
 import { useAuthentication } from "../Auth/Context";
 import Message from "../Message/Message";
 import Spinner from "../Spinner/Spinner";
+import { useHistory } from "react-router-dom";
 import { useAsync } from "../helpers";
-
-import Input from "../Input/Input";
-import Button from "../Button/Button";
+import NewPassword from './NewPassword'
 import "./user.css";
 
 const AccountVerify = (props) => {
-  const { verifyUserAccount } = useAuthentication();
-  const [showPage, noPage] = useState(false);
-  const [email, setEmail] = useState();
+  const { verifyUserAccount, updateUserPassword } = useAuthentication();
+  const [newPassword, setNewPassword] = useState(false);
 
+  let history = useHistory();
   let { id } = useParams();
 
   var formData = new FormData();
@@ -22,11 +21,13 @@ const AccountVerify = (props) => {
   const resource = useAsync(verifyUserAccount, formData);
   console.log(resource);
 
-  if (resource.message === "link is valid") {
-    setEmail(resource.value.email);
-  } else return;
+  const push = () => {
+    history.push("/newpassword");
+  };
 
-  console.log(email);
+  if (resource.message === "link is valid") {
+    push();
+  }
 
   return (
     <div className=" user_wrapper">
@@ -34,73 +35,13 @@ const AccountVerify = (props) => {
 
       {resource.loading ? (
         <Spinner />
-      ) : (
+      ) : resource.message ? (
         <Message message={resource.message} classname="message" />
-      )}
+      ) : null}
+
+      {newPassword && <NewPassword />}
     </div>
   );
 };
 
 export default AccountVerify;
-
-function Conponent({ email }) {
-  const { updateUserPassword } = useAuthentication();
-
-  const [newPassword, setNewPassword] = useState();
-  const [confirmPassword, setConfirmPassword] = useState();
-
-  const [message, setMessage] = useState();
-
-  const reset = () => {
-    setNewPassword("");
-    setConfirmPassword("");
-  };
-
-  const updatePassword = async (event) => {
-    setMessage();
-    event.preventDefault();
-    var formData = new FormData();
-
-    formData.set("buyer_unique_id", email);
-    formData.set("new_password", newPassword);
-    formData.set("confirm_password", confirmPassword);
-
-    const data = await updateUserPassword(formData);
-
-    if (data.data.error === true) {
-      setMessage(data.data.message);
-    } else if (data.data.error === false) {
-      reset();
-    }
-  };
-
-  return (
-    <div className=" user_wrapper">
-      <div className="page_title">Reset Password</div>
-
-      <Input
-        classname="input "
-        placeholder="New Password"
-        value={newPassword}
-        type="password"
-        action={(e) => setNewPassword(e.target.value)}
-      />
-
-      <Input
-        classname="input "
-        placeholder="Confirm Password"
-        value={confirmPassword}
-        type="password"
-        action={(e) => setConfirmPassword(e.target.value)}
-      />
-
-      <div className="message_wrapper ">
-        {message ? <Message message={message} classname="message" /> : null}
-      </div>
-
-      <div className="button_wrapper ">
-        <Button name="Submit" action={updatePassword} classname="primary" />
-      </div>
-    </div>
-  );
-}
