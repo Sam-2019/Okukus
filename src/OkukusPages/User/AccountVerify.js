6b76d73da5a45b68bd7218e829961156061ff7ed
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-10-19 14:24:22 +0000

    Account Reset Test

--- src/OkukusPages/User/AccountVerify.js
+++ src/OkukusPages/User/AccountVerify.js
@@ -5,8 +5,16 @@ import Message from "../Message/Message";
 import Spinner from "../Spinner/Spinner";
 import { useHistory } from "react-router-dom";
 import { useAsync } from "../helpers";
-import NewPassword from "./NewPassword";
+
+
+import Input from "../Input/Input";
+import Button from "../Button/Button";
 import "./user.css";
+
+
+
+
+
 
 const AccountVerify = (props) => {
   const { verifyUserAccount, updateUserPassword } = useAuthentication();
@@ -43,9 +51,72 @@ const AccountVerify = (props) => {
         <Message message={resource.message} classname="message" />
       )}
 
-      <div>{showPage ? <NewPassword email={email} /> : null}</div>
+      <div>{showPage === true ? <Conponent email={email} /> : null}</div>
     </div>
   );
 };
 
 export default AccountVerify;
+
+
+function Conponent({email}) {
+  const { updateUserPassword } = useAuthentication();
+
+  const [newPassword, setNewPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
+  const [message, setMessage] = useState();
+
+  const reset = () => {
+    setNewPassword("");
+    setConfirmPassword("");
+  };
+
+  const updatePassword = async (event) => {
+    setMessage();
+    event.preventDefault();
+    var formData = new FormData();
+
+    formData.set("buyer_unique_id", email);
+    formData.set("new_password", newPassword);
+    formData.set("confirm_password", confirmPassword);
+
+    const data = await updateUserPassword(formData);
+
+    if (data.data.error === true) {
+      setMessage(data.data.message);
+    } else if (data.data.error === false) {
+      reset();
+    }
+  };
+
+  return (
+    <div className=" user_wrapper">
+      <div className="page_title">Reset Password</div>
+
+      <Input
+        classname="input "
+        placeholder="New Password"
+        value={newPassword}
+        type="password"
+        action={(e) => setNewPassword(e.target.value)}
+      />
+
+      <Input
+        classname="input "
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        type="password"
+        action={(e) => setConfirmPassword(e.target.value)}
+      />
+
+      <div className="message_wrapper ">
+        {message ? <Message message={message} classname="message" /> : null}
+      </div>
+
+      <div className="button_wrapper ">
+        <Button name="Submit" action={updatePassword} classname="primary" />
+      </div>
+    </div>
+  );
+}
