5242978b3922e55235434b9a9cdf54080d331fb0
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-10-19 18:08:01 +0000

    added Account Verification

--- src/OkukusPages/User/UserVerify.js
+++ src/OkukusPages/User/UserVerify.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { useAuthentication } from "../Auth/Context";
 import Message from "../Message/Message";
 import Spinner from "../Spinner/Spinner";
+import Button from "../../Button/Button";
 import { useAsync } from "../helpers";
 import "./user.css";
 
 function UserVerify() {
   const { verifyReadEmail } = useAuthentication();
-
+  let history = useHistory();
   let { id } = useParams();
 
   var formData = new FormData();
@@ -25,6 +26,16 @@ function UserVerify() {
           <Message message={resource.message} classname="message" />
         </div>
       )}
+
+      <div className="button_wrapper ">
+        <Button
+          name="    Go Shopping"
+          action={() => {
+            history.push("/");
+          }}
+          classname="primary"
+        />
+      </div>
     </div>
   );
 }
