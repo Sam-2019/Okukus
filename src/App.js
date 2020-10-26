4832e78022acc82dd1b7a22df84f207207fb1dcd
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2020-10-26 16:30:48 +0000

    Pagination

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { ContextProvider } from "./OkukusPages/Auth/Context";
 import "bootstrap/dist/css/bootstrap.css";
-import Okukus from "./OkukusPages/Okukus";
-// import Future from "./Future1";
+import Okukus from './OkukusPages/Okukus'
 
 const App = () => {
   return (
